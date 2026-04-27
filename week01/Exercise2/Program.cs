35f52ef52b99c2b1da9b4ab3dcc13ba213dac094
Steven Rushing <steven@stevenrushing.com>
2026-04-27 14:55:22 +0200

    Completed week 1 exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What was your grade? ");
+        string userGrade = Console.ReadLine();
+        int userGradeInt = int.Parse(userGrade);
+        if (userGradeInt >= 90)
+        {
+            Console.WriteLine("You got an A");
+        }
+        else if (userGradeInt >= 80)
+        {
+            Console.WriteLine("You got a B");
+        }
+        else if (userGradeInt >= 70)
+        {
+            Console.WriteLine("You got a C");
+        }
+        else if (userGradeInt >= 60)
+        {
+            Console.WriteLine("You got an F");
+        }
+        else
+        {
+            Console.WriteLine("You failed the course");
+        }
     }
 }
+
+// A >= 90
+// B >= 80
+// C >= 70
+// D >= 60
+// F < 60
