20b55c26620349e0372146b63274b84348705cc9
Steven Rushing <steven@stevenrushing.com>
2026-05-05 15:01:43 +0200

    Very beginning of fraction assignment and added the health class powerpoint

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction x = new Fraction();
+        Fraction y = new Fraction(5);
+        Fraction z = new Fraction(2,4);
+
     }
 }
