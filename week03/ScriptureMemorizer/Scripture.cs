ef60451b09a2e58a310d81efeed866ac7f85905a
Steven Rushing <steven@stevenrushing.com>
2026-05-30 23:41:10 +0200

    Finished the assignment

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -12,4 +12,54 @@ public class Scripture
             _words.Add(new Word(word));
         }
     }
+
+    public string GetDisplayText()
+    {
+        string words = "";
+        foreach (Word word in _words)
+        {
+            words += word.GetDisplayText() + " ";
+        }
+        words = words.Trim();
+        return $"{_reference.GetDisplayText()} {words}";
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        List<Word> visibleWords = new List<Word>();
+        foreach (Word word in _words)
+        {
+            if (word.IsHidden() == false)
+            {
+                visibleWords.Add(word);
+            }
+        }
+
+        int hideCount = numberToHide;
+        if (visibleWords.Count < hideCount)
+        {
+            hideCount = visibleWords.Count;
+        }
+
+        for (int i = 0; i < hideCount; i++)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (word.IsHidden() == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
